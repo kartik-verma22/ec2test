e4c85dbf20dc4af9738d8bf77cb46cf4f475e1c8
Kartik verma <kartik.vermacs2021@indoreinstitute.com>
2024-08-03 23:03:47 +0530

    kartik

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const http = require("http");
 
 const myServer = http.createServer((req,res) =>{
     console.log("New Request Recived!!");
-    res.end("Hello From Server");
+    res.end("NOW WE ARE ON EC2 : CONGRATS IS SUCCESSFULL ");
+    
 });
 
 myServer.listen(9000, ()=> console.log("Server Started on port No. 9000"));
